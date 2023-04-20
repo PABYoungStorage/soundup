8aa260ad538c50d48b7facef6c8bd053d6e8e2e6
PABYoungStorage <auth.youngstorage@gmail.com>
2023-04-20 21:39:58 +0530

    login page updated

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <h1>welcome to the soundup</h1>
+    </>
+  );
+}
